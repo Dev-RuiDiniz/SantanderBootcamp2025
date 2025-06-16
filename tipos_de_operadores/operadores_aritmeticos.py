bea664cd0a9db0220cdb5bdebab97272cc64f975
Rui Franscisco de Paula <rui.pdiniz@gmail.com>
2025-06-16 08:13:12 -0300

    add aula 1 e aula 2
    add aula 1 - Operadores Aritméticos
    add aula 2 - Operadores de Comparação

---
+++ tipos_de_operadores/operadores_aritmeticos.py
@@ -0,0 +1,32 @@
+# Operadores básicos de Python: +, -, *, /, //, %, **
+
+valor_1 = 10
+valor_2 = 5
+
+# Soma
+soma = valor_1 + valor_2
+print("Soma:", soma)
+
+# Subtração
+subtracao = valor_1 - valor_2
+print("Subtração:", subtracao)
+
+# Multiplicação
+multiplicacao = valor_1 * valor_2
+print("Multiplicação:", multiplicacao)
+
+# Divisão
+divisao = valor_1 / valor_2
+print("Divisão:", divisao)
+
+# Divisão inteira
+divisao_inteira = valor_1 // valor_2
+print("Divisão inteira:", divisao_inteira)
+
+# Módulo (resto da divisão)
+modulo = valor_1 % valor_2
+print("Módulo:", modulo)
+
+# Exponenciação
+exponenciacao = valor_1 ** valor_2
+print("Exponenciação:", exponenciacao)
---
+++ tipos_de_operadores/operadores_de_comparacao.py
@@ -0,0 +1,28 @@
+# Operadores de comparação em Python: ==, !=, <, >, <=, >=
+
+valor_1 = 10
+valor_2 = 5
+
+# Igualdade
+igualdade = valor_1 == valor_2
+print("Igualdade:", igualdade)
+
+# Desigualdade
+desigualdade = valor_1 != valor_2
+print("Desigualdade:", desigualdade)
+
+# Menor que
+menor_que = valor_1 < valor_2
+print("Menor que:", menor_que)
+
+# Maior que
+maior_que = valor_1 > valor_2
+print("Maior que:", maior_que)
+
+# Menor ou igual a
+menor_ou_igual = valor_1 <= valor_2
+print("Menor ou igual a:", menor_ou_igual)
+
+# Maior ou igual a
+maior_ou_igual = valor_1 >= valor_2
+print("Maior ou igual a:", maior_ou_igual)
