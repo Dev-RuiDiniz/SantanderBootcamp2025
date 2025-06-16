6bb6d8aa0c5441376fc104895aac52956f256dd7
Rui Franscisco de Paula <rui.pdiniz@gmail.com>
2025-06-16 08:18:55 -0300

    Create operadores_de_atribuicao.py
    add aula 3 - Operadores de Atribuição

---
+++ tipos_de_operadores/operadores_de_atribuicao.py
@@ -0,0 +1,35 @@
+# Operadores de atribuição em Python: =, +=, -=, *=, /=, //=, %=, **=
+
+valor = 10
+
+# Atribuição simples
+valor_atribuido = valor
+print("Atribuição simples:", valor_atribuido)
+
+# Atribuição com adição
+valor_atribuido += 5  # Equivalente a valor = valor + 5
+print("Atribuição com adição:", valor_atribuido)
+
+# Atribuição com subtração
+valor_atribuido -= 3  # Equivalente a valor = valor - 3
+print("Atribuição com subtração:", valor_atribuido)
+
+# Atribuição com multiplicação
+valor_atribuido *= 2  # Equivalente a valor = valor * 2
+print("Atribuição com multiplicação:", valor_atribuido)
+
+# Atribuição com divisão
+valor_atribuido /= 4  # Equivalente a valor = valor / 4
+print("Atribuição com divisão:", valor_atribuido)
+
+# Atribuição com divisão inteira
+valor_atribuido //= 2  # Equivalente a valor = valor // 2
+print("Atribuição com divisão inteira:", valor_atribuido)
+
+# Atribuição com módulo
+valor_atribuido %= 3  # Equivalente a valor = valor % 3
+print("Atribuição com módulo:", valor_atribuido)
+
+# Atribuição com exponenciação
+valor_atribuido **= 2  # Equivalente a valor = valor ** 2
+print("Atribuição com exponenciação:", valor_atribuido)
