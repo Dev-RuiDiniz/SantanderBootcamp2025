860205575fca1b4511eb7ce0eb57e512f34a3656
Rui Franscisco de Paula <rui.pdiniz@gmail.com>
2025-06-17 11:57:50 -0300

    Create string_multiplas_linhas.py
    - Aula sobre String com múltiplas linhas.

---
+++ strings_e_fatiamento/string_multiplas_linhas.py
@@ -0,0 +1,18 @@
+# String em Python podem ser definidas em várias linhas usando aspas triplas (simples ou duplas).
+# Isso é útil para criar strings longas ou para incluir quebras de linha sem precisar usar o caractere de escape `\n`.
+
+# Exemplo de string em várias linhas
+def string_multiplas_linhas():
+    texto = """Esta é uma string
+que ocupa várias linhas.
+Ela pode ser usada para armazenar
+textos longos ou para incluir
+quebras de linha sem precisar
+usar o caractere de escape. \n"""
+    
+    print(texto)    
+    
+# Chamada da função de string em várias linhas
+string_multiplas_linhas()
+# Este código demonstra como criar uma string que ocupa várias linhas em Python usando aspas triplas.
+# Ele define uma string longa que inclui quebras de linha e imprime o resultado.
