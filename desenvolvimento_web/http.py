aacb1a3e7d3c3fb0c66b13dcdef0b3d96ab67732
Rui Franscisco de Paula <rui.pdiniz@gmail.com>
2025-07-07 09:28:11 -0300

    Create http.py

---
+++ desenvolvimento_web/http.py
@@ -0,0 +1,14 @@
+# O que é HTTP?
+# HTTP (Hypertext Transfer Protocol) é o protocolo de comunicação utilizado na web para a transferência de dados entre clientes (como navegadores) e servidores. Ele define como as mensagens são formatadas e transmitidas, além de como os servidores e navegadores devem responder a diferentes comandos. O HTTP é um protocolo sem estado, o que significa que cada requisição é independente e não mantém informações sobre requisições anteriores.
+
+# Verbos HTTP:
+# GET: Recupera dados de um recurso.
+# POST: Envia dados para criar um novo recurso.
+# PUT: Atualiza um recurso existente.
+# DELETE: Remove um recurso existente.
+# PATCH: Atualiza parcialmente um recurso existente.
+
+# Como funciona o HTTP?
+# O HTTP funciona através de um modelo cliente-servidor. O cliente (geralmente um navegador) envia uma requisição HTTP para o servidor, que processa a requisição e retorna uma resposta. A requisição contém informações como o método HTTP, o URL do recurso solicitado e cabeçalhos adicionais. A resposta do servidor inclui um código de status HTTP, que indica o resultado da requisição, e os dados solicitados (se aplicável).
+
+
